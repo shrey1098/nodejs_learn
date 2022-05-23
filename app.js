e77e62735cddb2354d98c0ad5d6983a270718c70
shrey1098 <shrey1098@hotmail.com>
2022-05-23 12:53:14 +0530

    new

--- app.js
+++ app.js
@@ -1,39 +1,11 @@
-const {readFile, writeFile} = require('fs').promises;
-// const util =require('util');
-// const readFilePromise = util.promisify(readFile)
-// const writeFilePromise = util.promisify(writeFile)
+var http = require('http');
+var fs =require('fs')
+const server = http.createServer()
 
-
-
-const start =async()=>{
-  try{
-    const first = await readFile('./content/first.txt', 'utf8');
-    const second = await readFile('./content/second.txt', 'utf8');
-    await writeFile(
-      './content/result-mind-grenade.txt',
-      `THIS IS AWESOME : ${first} ${second}`,
-      { flag: 'a' }
-    )
-    console.log(first, second)
-  }catch(error){
-    console.log(error)
-  }
-}
-start()
-
-// getText('./content/first.txt')
-// .then((result)=> console.log(result))
-// .catch((err)=>console.log(err))
-
-// const getText = (path)=>{
-//   return new Promise((resolve, reject)=>{
-//     readFile(path, 'utf8',(err, data)=>{
-//       if(err){
-//         reject(err)
-//       }else{
-//         resolve(data)
-//       }
-//     })
-
-//   })
-// }
+http
+  .createServer(function(req, res){
+    const text = fs.readFileSync('./content/big.txt', 'utf8')
+    res.end(text)
+    console.log('done');
+  })
+.listen(5000)
